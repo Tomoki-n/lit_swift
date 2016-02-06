6516615f3a3cf120ccf0879af8bd50c96cd92530
Nishinaka Tomoki <ariaqua713.f7z@gmail.com>
2016-02-06 14:41:39 +0900

    count comp

---
+++ count/test/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Nishinaka Tomoki on 2016/02/06.
+//  Copyright © 2016年 Nishinaka Tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var i = 0
+    @IBOutlet var tes: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var add: UIButton!
+
+    @IBAction func add(sender: AnyObject) {
+        i++
+        tes.text = String(i)
+    }
+}
+
